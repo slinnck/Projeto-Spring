9c31dc8f63903931802b0fb4c53bdf9778b9fa61
Misael Oliveira <misael.oliver2015@gmail.com>
2022-04-28 11:05:07 -0300

    Project Init

---
+++ src/main/java/com/example/projetosts/ProjetoStsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetosts/ProjetoStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
