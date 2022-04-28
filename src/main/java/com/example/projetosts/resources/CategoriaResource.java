a8c71bcaa828847630c52e9cacea5b1dce91af2c
Misael Oliveira <misael.oliver2015@gmail.com>
2022-04-28 19:35:22 -0300

    Classe domain - Categoria

--- src/main/java/com/example/projetosts/resources/CategoriaResource.java
+++ src/main/java/com/example/projetosts/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.example.projetosts.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.projetosts.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando!";
+	public List<Categoria> listar() {
+		Categoria obj = new Categoria(1, "Tecnologia da Informação");
+		Categoria obj2 = new Categoria(2, "Cinema");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(obj);
+		list.add(obj2);
+		
+		return list;
 	}
 
 }
