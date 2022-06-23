2b0697a4430219ef166e178105eca4c75f178376
slinnck <misael.oliver2015@gmail.com>
2022-06-23 10:47:07 -0300

    Atualização de annoation: Uso único de @JsonIgnore, remoção de @JsonManegedReference & JsonBackReference

--- src/main/java/projetosts/ProjetoStsApplication.java
+++ src/main/java/projetosts/ProjetoStsApplication.java
@@ -91,7 +91,7 @@ public class ProjetoStsApplication implements CommandLineRunner {
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
-		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c1 = new Cidade(null, "Winterfell", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
